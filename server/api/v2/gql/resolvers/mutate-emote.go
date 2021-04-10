f00f43a3ab91bebf7fbabe085a422c67bd29ae5a
Anatole <anatole@xiantis.net>
2021-04-10 22:03:01 +0200

    API/GQL - Mutate Channel Emote resolves user

--- server/api/v2/gql/resolvers/mutate-emote.go
+++ server/api/v2/gql/resolvers/mutate-emote.go
@@ -2,12 +2,9 @@ package resolvers
 
 import (
 	"context"
-	"fmt"
-	"reflect"
 	"time"
 
 	"github.com/SevenTV/ServerGo/mongo"
-	"github.com/SevenTV/ServerGo/redis"
 	"github.com/SevenTV/ServerGo/utils"
 	"github.com/SevenTV/ServerGo/validation"
 	log "github.com/sirupsen/logrus"
@@ -143,6 +140,11 @@ func (*RootResolver) EditEmote(ctx context.Context, args struct {
 		}
 	}
 
+	field, failed := GenerateSelectedFieldMap(ctx, maxDepth)
+	if failed {
+		return nil, errDepth
+	}
+
 	if len(logChanges) > 0 {
 		update["last_modified_date"] = time.Now()
 
@@ -172,159 +174,7 @@ func (*RootResolver) EditEmote(ctx context.Context, args struct {
 		if err != nil {
 			log.Errorf("mongo, err=%v", err)
 		}
-		return &emoteResolver{
-			v: emote,
-		}, nil
+		return GenerateEmoteResolver(ctx, emote, &emote.ID, field.children)
 	}
-	return &emoteResolver{
-		v: emote,
-	}, nil
-}
-
-func updateStructFields(struc interface{}) interface{} {
-	val := reflect.ValueOf(struc)
-	jsonMap := make(map[string]interface{}, val.Type().NumField())
-
-	for i := 0; i < val.Type().NumField(); i++ {
-		f := val.Type().Field(i)
-		fmt.Println("COCK", f.Name)
-	}
-
-	return jsonMap
-}
-
-//
-// Mutate Emote - Add to Channel
-//
-func (*RootResolver) AddChannelEmote(ctx context.Context, args struct {
-	ChannelID string
-	EmoteID   string
-	Reason    *string
-}) (*response, error) {
-	usr, ok := ctx.Value(utils.UserKey).(*mongo.User)
-	if !ok {
-		return nil, errLoginRequired
-	}
-
-	emoteID, err := primitive.ObjectIDFromHex(args.EmoteID)
-	if err != nil {
-		return nil, errUnknownEmote
-	}
-
-	channelID, err := primitive.ObjectIDFromHex(args.ChannelID)
-	if err != nil {
-		return nil, errUnknownChannel
-	}
-
-	_, err = redis.Client.HGet(redis.Ctx, "user:bans", channelID.Hex()).Result()
-	if err != nil && err != redis.ErrNil {
-		log.Errorf("redis, err=%v", err)
-		return nil, errInternalServer
-	}
-
-	if err == nil {
-		return nil, errChannelBanned
-	}
-
-	res := mongo.Database.Collection("users").FindOne(mongo.Ctx, bson.M{
-		"_id": channelID,
-	})
-
-	channel := &mongo.User{}
-
-	err = res.Err()
-
-	if err == nil {
-		err = res.Decode(channel)
-	}
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return nil, errUnknownChannel
-		}
-		log.Errorf("mongo, err=%v", err)
-		return nil, errInternalServer
-	}
-
-	if !mongo.UserHasPermission(usr, mongo.RolePermissionManageUsers) {
-		if channel.ID.Hex() != usr.ID.Hex() {
-			found := false
-			for _, e := range channel.EditorIDs {
-				if e.Hex() == usr.ID.Hex() {
-					found = true
-					break
-				}
-			}
-			if !found {
-				return nil, errAccessDenied
-			}
-		}
-	}
-
-	for _, eID := range channel.EmoteIDs {
-		if eID.Hex() == emoteID.Hex() {
-			return &response{
-				Status:  200,
-				Message: "no change",
-			}, nil
-		}
-	}
-
-	emoteRes := mongo.Database.Collection("emotes").FindOne(mongo.Ctx, bson.M{
-		"_id":    emoteID,
-		"status": mongo.EmoteStatusLive,
-	})
-
-	emote := &mongo.Emote{}
-	err = emoteRes.Err()
-	if err == nil {
-		err = emoteRes.Decode(emote)
-	}
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return nil, errUnknownEmote
-		}
-		log.Errorf("mongo, err=%v", err)
-		return nil, errInternalServer
-	}
-
-	sharedWith := []string{}
-	for _, v := range emote.SharedWith {
-		sharedWith = append(sharedWith, v.Hex())
-	}
-	if utils.HasBits(int64(emote.Visibility), int64(mongo.EmoteVisibilityPrivate)) {
-		if emote.OwnerID.Hex() != channelID.Hex() || !utils.Contains(sharedWith, emoteID.Hex()) {
-			return nil, errUnknownEmote
-		}
-	}
-
-	emoteIDs := append(channel.EmoteIDs, emoteID)
-	_, err = mongo.Database.Collection("users").UpdateOne(mongo.Ctx, bson.M{
-		"_id": channelID,
-	}, bson.M{
-		"$set": bson.M{
-			"emotes": emoteIDs,
-		},
-	})
-	if err != nil {
-		log.Errorf("mongo, err=%v", err)
-		return nil, errInternalServer
-	}
-
-	_, err = mongo.Database.Collection("audit").InsertOne(mongo.Ctx, &mongo.AuditLog{
-		Type:      mongo.AuditLogTypeUserChannelEmoteAdd,
-		CreatedBy: usr.ID,
-		Target:    &mongo.Target{ID: &channelID, Type: "users"},
-		Changes: []*mongo.AuditLogChange{
-			{Key: "emotes", OldValue: channel.EmoteIDs, NewValue: emoteIDs},
-		},
-		Reason: args.Reason,
-	})
-	if err != nil {
-		log.Errorf("mongo, err=%v", err)
-	}
-
-	return &response{
-		Status:  200,
-		Message: "success",
-	}, nil
+	return GenerateEmoteResolver(ctx, emote, &emote.ID, field.children)
 }
