8fbc86979f349b391197e5935100019ac0046927
Anatole <anatole@xiantis.net>
2021-05-26 09:36:43 +0200

    API - Chatterino version endpoint

--- src/server/api/v2/v2.go
+++ src/server/api/v2/v2.go
@@ -1,10 +1,16 @@
 package v2
 
 import (
+	"time"
+
+	"github.com/SevenTV/ServerGo/src/jwt"
+	"github.com/SevenTV/ServerGo/src/server/api/v2/chatterino"
 	"github.com/SevenTV/ServerGo/src/server/api/v2/emotes"
 	"github.com/SevenTV/ServerGo/src/server/api/v2/gql"
 	api_websocket "github.com/SevenTV/ServerGo/src/server/api/v2/websocket"
+	"github.com/SevenTV/ServerGo/src/server/middleware"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func API(app fiber.Router) fiber.Router {
@@ -14,6 +20,31 @@ func API(app fiber.Router) fiber.Router {
 	Twitch(api)
 	emotes.Emotes(api)
 	gql.GQL(api)
+	chatterino.Chatterino(api)
+
+	// Debug
+	app.Get("/debuguser/:user/:twid", func(c *fiber.Ctx) error {
+		var oid primitive.ObjectID
+		userID := c.Params("user")
+		twid := c.Params("twid")
+		if id, err := primitive.ObjectIDFromHex(userID); err == nil {
+			oid = id
+		}
+
+		authPl := &middleware.PayloadJWT{
+			ID:           oid,
+			TWID:         twid,
+			TokenVersion: "1",
+			CreatedAt:    time.Now(),
+		}
+
+		tok, err := jwt.Sign(authPl)
+		if err != nil {
+			return c.Status(401).Send([]byte(err.Error()))
+		}
+
+		return c.Status(201).SendString(tok)
+	})
 
 	return api
 }
