c9ade67744306e3944be3aecfa3a998aa8f7fc99
Anatole <me@anatole.dev>
2021-07-01 01:15:31 +0200

    API/REST - `/platforms/:platform/:variant?` route

--- src/server/api/v2/rest/rest.go
+++ src/server/api/v2/rest/rest.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/SevenTV/ServerGo/src/configure"
+	"github.com/SevenTV/ServerGo/src/redis"
 	"github.com/SevenTV/ServerGo/src/server/api/v2/rest/badges"
 	"github.com/SevenTV/ServerGo/src/server/api/v2/rest/emotes"
 	"github.com/SevenTV/ServerGo/src/server/api/v2/rest/users"
@@ -53,6 +54,57 @@ func RestV2(app fiber.Router) fiber.Router {
 		return c.Send(j)
 	})
 
+	restGroup.Get("/platforms/:id/:variant?", func(c *fiber.Ctx) error {
+		ctx := c.Context()
+
+		platformID := c.Params("id")
+		variantID := c.Params("variant")
+		key := fmt.Sprintf("hits:platform:%v", platformID)
+		if variantID != "" {
+			key = key + fmt.Sprintf(":%v", variantID)
+		}
+
+		// Find platform
+		var platforms []*Platform
+		if err := configure.Config.UnmarshalKey("platforms", &platforms); err != nil {
+			return c.Status(500).SendString("Error decoding the config")
+		}
+
+		// Find the requested platform
+		var platform *Platform
+		var variant *PlatformVariant
+		for _, p := range platforms {
+			if p.ID != platformID {
+				continue
+			}
+
+			platform = p
+			if p.Variants != nil {
+				for _, v := range *p.Variants {
+					if v.ID != variantID {
+						continue
+					}
+
+					variant = &v
+					break
+				}
+			}
+			break
+		}
+		if platform == nil {
+			return c.Status(400).SendString("Unknown Platform")
+		}
+
+		redis.Client.Incr(ctx, key)
+		if variant != nil {
+			return c.Redirect(variant.URL)
+		} else if platform != nil && platform.URL != "" {
+			return c.Redirect(platform.URL, 301)
+		}
+
+		return c.Redirect(configure.Config.GetString("website_url"))
+	})
+
 	return nil
 }
 
@@ -61,15 +113,18 @@ type PlatformsResult struct {
 }
 
 type Platform struct {
-	ID          string             `mapstructure:"id" json:"id"`
-	VersionTag  string             `mapstructure:"version_tag" json:"version_tag"`
-	New         bool               `mapstructure:"new" json:"new"`
-	Subversions *[]PlatformVariant `mapstructure:"variants" json:"variants"`
+	ID         string             `mapstructure:"id" json:"id"`
+	VersionTag string             `mapstructure:"version_tag" json:"version_tag"`
+	New        bool               `mapstructure:"new" json:"new"`
+	URL        string             `mapstructure:"url" json:"url"`
+	Variants   *[]PlatformVariant `mapstructure:"variants" json:"variants"`
 }
 
 type PlatformVariant struct {
-	Name    string `json:"name" mapstructure:"name"`
-	Author  string `json:"author" mapstructure:"author"`
-	Version string `json:"version" mapstructure:"version"`
-	URL     string `json:"url" mapstructure:"url"`
+	Name        string `json:"name" mapstructure:"name"`
+	ID          string `json:"id" mapstructure:"id"`
+	Author      string `json:"author" mapstructure:"author"`
+	Version     string `json:"version" mapstructure:"version"`
+	Description string `json:"description" mapstructure:"description"`
+	URL         string `json:"url" mapstructure:"url"`
 }
