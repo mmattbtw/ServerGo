fc9386af07671e49b8736dc1f7098e56cc1aecdf
Anatole <me@anatole.dev>
2021-07-13 06:10:35 +0200

    API/REST - Endpoint to convert WEBP emotes to GIF (#59)
    * API/REST - Endpoint to convert WEBP emotes to GIF
    
    * Destroy magic wand after completion

--- src/server/api/v2/rest/emotes/get-emote.go
+++ src/server/api/v2/rest/emotes/get-emote.go
@@ -1,11 +1,16 @@
 package emotes
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gopkg.in/gographics/imagick.v3/imagick"
 
 	"github.com/SevenTV/ServerGo/src/cache"
 	"github.com/SevenTV/ServerGo/src/configure"
@@ -15,6 +20,7 @@ import (
 	"github.com/SevenTV/ServerGo/src/server/middleware"
 	"github.com/SevenTV/ServerGo/src/utils"
 	"github.com/gofiber/fiber/v2"
+	log "github.com/sirupsen/logrus"
 )
 
 const chunkSize = 1024 * 1024
@@ -66,43 +72,53 @@ func GetEmoteRoute(router fiber.Router) {
 			return c.Send(b)
 		})
 
-	// OEmbed
-	router.Get("/oembed/:emote.json", func(c *fiber.Ctx) error {
+	// Convert an emote in the CDN from WEBP or other format into PNG
+	rl := configure.Config.GetIntSlice("limits.route.emote-convert")
+	var m func(*fiber.Ctx) error
+	if rl != nil {
+		m = middleware.RateLimitMiddleware("emote-convert", int32(rl[0]), time.Millisecond*time.Duration(rl[1]))
+	}
+
+	router.Use("/:emote/convert.gif", m)
+	router.Get("/:emote/convert.gif", func(c *fiber.Ctx) error {
 		emoteID := c.Params("emote") // Get the emote ID parameter
-		pageTitle := c.Query("page-title", "7TV")
 
-		// Get the emote's data from DB
-		var emote *datastructure.Emote
-		var owner *datastructure.User
-		if id, err := primitive.ObjectIDFromHex(emoteID); err == nil {
-			if err := cache.FindOne(c.Context(), "emotes", "", bson.M{
-				"_id": id,
-			}, &emote); err != nil {
-				return c.Status(400).Send([]byte("Unknown Emote: " + err.Error()))
-			}
-			if err := cache.FindOne(c.Context(), "users", "", bson.M{
-				"_id": emote.OwnerID,
-			}, &owner); err != nil {
-				owner = &datastructure.User{}
-			}
+		// Create a new magick wand
+		wand := imagick.NewMagickWand()
+		defer wand.Destroy()
 
-			c.Set("Content-Type", "application/json")
+		// Get CDN URL
+		url := utils.GetCdnURL(emoteID, 3)
 
-			b, err := json.Marshal(OEmbedData{
-				Title:        pageTitle,
-				AuthorName:   owner.DisplayName,
-				AuthorURL:    utils.GetEmotePageURL(emote.ID.Hex()),
-				ProviderName: "7TV.APP - It's like a third party thing",
-				ProviderURL:  configure.Config.GetString("website_url"),
-			})
-			if err != nil {
-				return c.Status(400).Send([]byte(err.Error()))
-			}
-
-			return c.Status(200).Send(b)
-		} else {
-			return c.Status(400).Send([]byte(err.Error()))
+		// Download the image from the CDN
+		res, err := http.Get(url)
+		if err != nil {
+			log.WithError(err).Error("http")
+			return restutil.ErrAccessDenied().Send(c, fmt.Sprintf("Couldn't get file: %v", err.Error()))
 		}
+		defer res.Body.Close()
+		if res.StatusCode != 200 { // Check status
+			return restutil.ErrAccessDenied().Send(c, fmt.Sprintf("CDN returned non-200 status code (%d %v)", res.StatusCode, res.Status))
+		}
+
+		// Read response body and append to a byte slice
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			return restutil.ErrAccessDenied().Send(c, fmt.Sprintf("Failed to read file: %v", err.Error()))
+		}
+
+		// Add image to the magick wand
+		if err = wand.ReadImageBlob(b); err != nil {
+			log.WithError(err).Error("could not decode image")
+			return restutil.ErrBadRequest().Send(c, fmt.Sprintf("Couldn't decode image: %v", err.Error()))
+		}
+
+		// Convert & stream back to client
+		wand.SetIteratorIndex(0)
+		wand.SetImageFormat("gif")
+		wand.ResetIterator()
+		c.Set("Content-Type", "image/gif")
+		return c.SendStream(bytes.NewReader(wand.GetImagesBlob()))
 	})
 }
 
