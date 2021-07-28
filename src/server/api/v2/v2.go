71779c47ad2818051251f4cf4ccb1b701efa139a
Anatole <me@anatole.dev>
2021-07-28 02:20:42 +0200

    Fix potential vulnerability

--- src/server/api/v2/v2.go
+++ src/server/api/v2/v2.go
@@ -1,16 +1,11 @@
 package v2
 
 import (
-	"time"
-
-	"github.com/SevenTV/ServerGo/src/jwt"
 	"github.com/SevenTV/ServerGo/src/server/api/v2/chatterino"
 	"github.com/SevenTV/ServerGo/src/server/api/v2/gql"
 	"github.com/SevenTV/ServerGo/src/server/api/v2/rest"
 	api_websocket "github.com/SevenTV/ServerGo/src/server/api/v2/websocket"
-	"github.com/SevenTV/ServerGo/src/server/middleware"
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func API(app fiber.Router) fiber.Router {
@@ -21,30 +16,6 @@ func API(app fiber.Router) fiber.Router {
 	rest.RestV2(api)
 	gql.GQL(api)
 	chatterino.Chatterino(api)
-
-	// Debug
-	app.Get("/debuguser/:user/:twid", func(c *fiber.Ctx) error {
-		var oid primitive.ObjectID
-		userID := c.Params("user")
-		twid := c.Params("twid")
-		if id, err := primitive.ObjectIDFromHex(userID); err == nil {
-			oid = id
-		}
-
-		authPl := &middleware.PayloadJWT{
-			ID:           oid,
-			TWID:         twid,
-			TokenVersion: "1",
-			CreatedAt:    time.Now(),
-		}
-
-		tok, err := jwt.Sign(authPl)
-		if err != nil {
-			return c.Status(401).Send([]byte(err.Error()))
-		}
-
-		return c.Status(201).SendString(tok)
-	})
 
 	return api
 }
